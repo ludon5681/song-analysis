c4ad5a5b6f23dac731e7830c5990fe48e51f4140
ludon5681 <84600299+ludon5681@users.noreply.github.com>
2022-11-30 23:16:59 -0500

    Added landing page

---
+++ src/script.js
@@ -0,0 +1,23 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+$(function() {
+    //$("body").overlayScrollbars({ });
+
+    $("#album-cover, .album-text, .subheading").on("mouseenter", async function() {
+        $("#album-cover").addClass("hovered");
+        await sleep(150);
+        $(".album-text, p.subheading").removeClass("invisible");
+    })
+    $("#album-cover, .album-text, .subheading").on("mouseleave", async function() {
+        $("#album-cover").removeClass("hovered");
+        await sleep(150);
+        $(".album-text, p.subheading").addClass("invisible");
+    })
+    $("#album-cover, .album-text, .subheading").on("click", async function() {
+        $("body").addClass("pageChange");
+        await sleep(500);
+        window.location = "./options/options.html"
+    })
+})
